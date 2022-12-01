24aa31bb6ddd91065404d564ad9aaa859acd9694
= <vemund.hatlen@gmail.com>
2022-12-01 07:14:48 +0100

    Day 1 - Both stars

---
+++ day1/main.py
@@ -0,0 +1,11 @@
+elves = []
+with open('./inputdata.txt', 'r') as f:
+    rawin = f.read()
+    elvesitems = rawin.split("\n\n")
+    for i in elvesitems:
+        elves.append(sum([int(ii) for ii in i.split("\n")]))
+elves.sort()
+print(elves[-1])
+
+# Part Two
+print(elves[-1] + elves[-2] + elves[-3])
